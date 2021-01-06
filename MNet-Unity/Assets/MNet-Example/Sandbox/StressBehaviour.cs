f51497d69eb82c6f13783b7f1a9685805b146db7
Moe-Baker <moe4baker@gmail.com>
2021-01-06 19:47:14 +0200

    Work

---
+++ MNet-Unity/Assets/MNet-Example/Sandbox/StressBehaviour.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.AI;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditorInternal;
+#endif
+
+using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
+
+namespace MNet.Example
+{
+	public class StressBehaviour : NetworkBehaviour
+	{
+		public float interval = 0.1f;
+
+		string var = default;
+		[SyncVar(Authority = RemoteAuthority.Owner)]
+		public string Var
+		{
+			get => var;
+			set
+			{
+				var = value;
+			}
+		}
+
+		[NetworkRPC(Authority = RemoteAuthority.Owner)]
+		void Call(string arg, RpcInfo infp)
+        {
+
+        }
+
+		void Start()
+		{
+			if(Entity.IsMine) StartCoroutine(Procedure());
+		}
+
+		IEnumerator Procedure()
+		{
+			while (Entity.IsConnected)
+			{
+				SyncVar("Var", Var, "Hello World");
+				BroadcastRPC(Call, "Hello World");
+
+				yield return new WaitForSeconds(interval);
+			}
+		}
+	}
+}
